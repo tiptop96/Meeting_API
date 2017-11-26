d16a6b70e60eb1ca5bad9c57b388fd6a7819a712
tiptop96 <gustav@eiman.net>
2017-11-26 21:19:55 +0100

    Stuff

--- MeetingAPI/settings.py
+++ MeetingAPI/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'MIND_YOUR_BIZZZ'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.8']
+ALLOWED_HOSTS = ['192.168.0.8', '188.166.70.137']
 
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
